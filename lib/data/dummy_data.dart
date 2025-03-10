f9dd456136536ad515120a66bb4a36e315e6dc23
bagoesrex <bagoesrex@gmail.com>
2025-03-10 21:57:39 +0700

    feat: add category's model and dummy data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/models/category.dart';
+
+const availableCategories = [
+  Category(id: 'c1', title: 'Rendang', color: Colors.brown),
+  Category(id: 'c2', title: 'Sate', color: Colors.red),
+  Category(id: 'c3', title: 'Nasi Goreng', color: Colors.orange),
+  Category(id: 'c4', title: 'Gado-Gado', color: Colors.green),
+  Category(id: 'c5', title: 'Soto', color: Colors.yellow),
+  Category(id: 'c6', title: 'Pempek', color: Colors.blue),
+  Category(id: 'c7', title: 'Bakso', color: Colors.grey),
+  Category(id: 'c8', title: 'Gudeg', color: Colors.purple),
+  Category(id: 'c9', title: 'Rawon', color: Colors.black),
+  Category(id: 'c10', title: 'Es Cendol', color: Colors.teal),
+];
