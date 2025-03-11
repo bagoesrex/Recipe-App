cee4be50b682a0c213c4c2d250080ac682967dcc
bagoesrex <bagoesrex@gmail.com>
2025-03-12 02:26:25 +0700

    feat: add recipe item trait widget

--- lib/widgets/recipe_item.dart
+++ lib/widgets/recipe_item.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/models/recipe.dart';
+import 'package:recipe_app/widgets/recipe_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class RecipeItem extends StatelessWidget {
   const RecipeItem({super.key, required this.recipe});
 
   final Recipe recipe;
+
+  String get complexityText {
+    return recipe.complexity.name[0].toUpperCase() +
+        recipe.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return recipe.affordability.name[0].toUpperCase() +
+        recipe.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,9 +61,25 @@ class RecipeItem extends StatelessWidget {
                       ),
                     ),
                     const SizedBox(height: 12),
-                    Row(children: [
-                      
-                  ],),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RecipeItemTrait(
+                          icon: Icons.schedule,
+                          label: '${recipe.duration} min',
+                        ),
+                        const SizedBox(width: 12),
+                        RecipeItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                        const SizedBox(width: 12),
+                        RecipeItemTrait(
+                          icon: Icons.attach_money,
+                          label: affordabilityText,
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
