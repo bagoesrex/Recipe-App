7cc3a854c98278c8859d27e135ed3bd015e5bf92
bagoesrex <bagoesrex@gmail.com>
2025-03-18 20:49:58 +0700

    feat: create filters provier

---
+++ lib/provider/filters_provider.dart
@@ -0,0 +1,23 @@
+import 'package:riverpod/riverpod.dart';
+
+enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
+
+class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
+  FiltersNotifier()
+    : super({
+        Filter.glutenFree: false,
+        Filter.lactoseFree: false,
+        Filter.vegetarian: false,
+        Filter.vegan: false,
+      });
+
+  void setFilter(Filter filter, bool isActive) {
+    // state[active] = isActive;
+    state = {...state, filter: isActive};
+  }
+}
+
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+      (ref) => FiltersNotifier(),
+    );
